c45fa87fe2bd7096eeb18226386e66405585489a
ferka123 <ferka911@gmail.com>
2022-11-05 19:19:30 +0200

    feat: 04-copy-directory task implementation

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require("fs/promises");
+const path = require("path");
 
+async function copyDir(src, dest) {
+  try {
+    const destDirPath = path.resolve(__dirname, dest);
+    const destDir = await fs.mkdir(destDirPath, { recursive: true });
+    const srcDirPath = path.resolve(__dirname, src);
+    const srcDir = await fs.readdir(srcDirPath, {
+      withFileTypes: true,
+    });
+
+    for (const entity of srcDir)
+      if (entity.isFile()) {
+        const srcFilePath = path.join(srcDirPath, entity.name);
+        const destFilePath = path.join(destDirPath, entity.name);
+        await fs.copyFile(srcFilePath, destFilePath);
+      } else {
+        copyDir(path.join(src, entity.name), path.join(dest, entity.name));
+      }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyDir("files", "files-copy");
